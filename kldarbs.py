b59e2add9ce99b296b941aa9a4aaa2e521e7b961
ImantsD10a <rolands69dim@gmail.com>
2025-05-23 09:59:41 +0300

    Add files via upload

---
+++ kldarbs.py
@@ -0,0 +1,33 @@
+import random
+import time
+
+def sveiciens():
+  vards = input ("Ievadi savu vārdu:")
+  print (f"Sveiks, {vards}!")
+  return vards
+
+def min_skaitli(min_skait):
+  pareizs = random.randint (1,10)
+  if pareizs == min_skait:
+      print("Apsveicu, tu uzminēji pareizi!")
+  else:
+      print(f"Nepareizi, pareizs skaitlis bija {pareizs}.")
+      
+def prog():
+    sakums = time.time()
+    vards = sveiciens() 
+    while True:
+     
+        
+        
+    
+    
+    
+    
+    
+    
+     uzminets = False
+     break
+sveiciens()
+x = int(input("Min skaitli no 1 līdz 10: ") )
+min_skaitli(x)
